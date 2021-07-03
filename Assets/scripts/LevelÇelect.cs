e5299667fb9ec25ea8c5db45bbfc1a5e076f5ae7
Hqw <hqwgamer@gmail.com>
2021-07-03 11:01:51 -0300

    sletor de fases e menu de pausa(ui)

---
+++ Assets/scripts/LevelÇelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelÇelect : MonoBehaviour
+{
+   public List <Button> fases;
+   
+   public void LevelChange()
+   {
+        //SceneManager.LoadScene(fases - 1);
+    
+   }
+}
