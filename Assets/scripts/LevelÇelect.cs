6aa980841a5ddaa72d8784be7a577ec5313163ad
Hqw <hqwgamer@gmail.com>
2021-08-07 11:01:05 -0300

    levelçelect e alguns bugs

--- Assets/scripts/LevelÇelect.cs
+++ Assets/scripts/LevelÇelect.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class LevelÇelect : MonoBehaviour
 {
-   public List <Button> fases;
-   
-   public void LevelChange()
-   {
-        //SceneManager.LoadScene(fases - 1);
+    public List <GameObject> fases;
+    GameObject selectedLevel;
+    public void Start() 
+    {
+        foreach (Transform child in transform) 
+        {
+            fases.Add(child.gameObject);
+        }
+
+    } 
+
     
-   }
+    public void LevelChange()
+    {
+        selectedLevel = EventSystem.current.currentSelectedGameObject;
+        SceneManager.LoadScene(fases.IndexOf(selectedLevel.gameObject)+2);
+
+    }
 }
